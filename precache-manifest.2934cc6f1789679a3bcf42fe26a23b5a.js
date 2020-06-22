4a9a3582d792df6863d5f559ea0e15fdc127b13d
dolugen <dolugen@users.noreply.github.com>
2020-06-22 14:40:13 +0000

    Deploying to gh-pages from  @ 3411052e573ecf387450c70f267a0ffe0abc5d3d 🚀

---
+++ precache-manifest.2934cc6f1789679a3bcf42fe26a23b5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b6001d7a8510e7620f048e18e2a05d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8753420b42e4fd2df3d",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "97d2465a3ef41b4fa456",
+    "url": "/static/js/2.8c452cad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8c452cad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8753420b42e4fd2df3d",
+    "url": "/static/js/main.2e643d67.chunk.js"
+  },
+  {
+    "revision": "f14db792499cf854dac3",
+    "url": "/static/js/runtime-main.4a2c76fe.js"
+  }
+]);
